5f7aff4f0a6a42514f9594d3e27b770d1f916668
CompleteTheSquare <loyal.lugia@gmail.com>
2019-04-02 23:48:10 -0400

    Initial commit
    Assignment 1

---
+++ Assignment 1 - About Me/src/aboutme/AboutMe.java
@@ -0,0 +1,21 @@
+//Assignment 1 - About Me
+package aboutme;
+
+/**
+ *
+ */
+public class AboutMe {
+
+public static String name = "CompleteTheSquare";
+public static String animal = "cats";
+public static String tvShow = "Big Bang Theory";
+public static String musician = "Beethoven";
+
+    public static void main(String[] args) {
+System.out.println("My name is _.".replace("_", name));
+System.out.println("My favourite animal is _.".replace("_", animal));
+System.out.println("My favourite TV show is _.".replace("_", tvShow));
+System.out.println("My favourite musician is _.".replace("_", musician));
+    }
+    
+}
