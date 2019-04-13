addc2578077e024575fa6a30bc8b36638fe0bdfc
CompleteTheSquare <loyal.lugia@gmail.com>
2019-04-13 12:28:17 -0400

    Final push
    Fixed bugs and made program shorter

--- Assignment 10 - Sandwich Sort/src/sandwichsort4/SandwichSort4.java
+++ Assignment 10 - Sandwich Sort/src/sandwichsort4/SandwichSort4.java
@@ -1,135 +1,83 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package sandwichsort4;
 
 import java.util.Arrays;
 
 public class SandwichSort4 {
 
+// these are discuss later but critical to program's successful run
     public static int highAdjustment = 1;//holds the upper bounds in array
     public static int lowAdjustment = 0;//holds lower bounds in array
 
+    /**
+     * @param intArray the inputted integer array
+     */
+    //to see intermediates of the sort, uncomment the lines with 4 forward slashes
     public static void sandwichSort(int[] intArray) {
 
-        int indexOfHighest = 0, indexOfLowest = 0;
-        int highest = intArray[intArray.length / 2];
-        int lowest = intArray[intArray.length / 2];
-
+        //prints the user's original array out
         System.out.println("Original Array: " + Arrays.toString(intArray));
-        System.out.println ("--------------------------------");
-        //getHighest1
-        for (int index = lowAdjustment; index <= intArray.length - highAdjustment; index++) {
+        System.out.println("--------------------------------");
 
-            if (intArray[index] >= highest) {
-                highest = intArray[index];
-                indexOfHighest = index;
+        // makes sure you swap until the entire array is sorted.
+        for (int numberOfRepeats = 0; numberOfRepeats < intArray.length / 2; numberOfRepeats++) {
+
+            //set comparing point to the middle of the array and default starting indexes to 0
+            int indexOfHighest = 0, indexOfLowest = 0;
+            int highest = intArray[intArray.length / 2];
+            int lowest = intArray[intArray.length / 2];
+
+            ////System.out.println("Starting with: " + Arrays.toString(intArray));
+            //get the highest element between the middle elements, excluding the outer elements each iteration                      
+            for (int index = lowAdjustment; index <= intArray.length - highAdjustment; index++) {
+
+                if (intArray[index] >= highest) {
+                    highest = intArray[index];
+                    indexOfHighest = index;
+                }
             }
-        }
-        System.out.println("\n");
-        System.out.println("Index of highest : " + indexOfHighest);
-        System.out.println("Value of highest: " + highest);
-        swap(intArray, indexOfHighest, intArray.length - highAdjustment);
-        System.out.println(Arrays.toString(intArray));
 
-        //getLowest1
-        for (int index = lowAdjustment; index <= intArray.length - highAdjustment; index++) {
-            if (intArray[index] <= lowest) {
-                lowest = intArray[index];
-                indexOfLowest = index;
+            //swaps the highest element with the element in new highest position
+            swap(intArray, indexOfHighest, intArray.length - highAdjustment);
+            ////System.out.println("Swapping highest: " + Arrays.toString(intArray));
+
+            //get the lowest element between the middle elements, excluding the outer elements each iteration    
+            for (int index = lowAdjustment; index <= intArray.length - highAdjustment; index++) {
+                if (intArray[index] <= lowest) {
+                    lowest = intArray[index];
+                    indexOfLowest = index;
+                }
             }
+
+            swap(intArray, indexOfLowest, lowAdjustment);
+            ////System.out.println("Swapping Lowest: " + Arrays.toString(intArray));
+            ////System.out.println("--------------------------------");
+
+            //for every iteration of the array, the array being worked on gets smaller
+            //these make sure the array gets smaller for each iteration else Stack Overflow Error
+            lowAdjustment++;
+            highAdjustment++;
         }
-        System.out.println("\n");
-        System.out.println("Index of lowest : " + indexOfLowest);
-        System.out.println("Value of lowest: " + lowest);
-        swap(intArray, indexOfLowest, lowAdjustment);
-        System.out.println(Arrays.toString(intArray));
-                System.out.println ("--------------------------------");
 
-        lowAdjustment++;
-        highAdjustment++;
-
-        indexOfHighest = 0;
-        indexOfLowest = 0;
-        highest = intArray[intArray.length / 2];
-        lowest = intArray[intArray.length / 2];
-                //getHighest2
-        for (int index = lowAdjustment; index <= intArray.length - highAdjustment; index++) {
-
-            if (intArray[index] >= highest) {
-                highest = intArray[index];
-                indexOfHighest = index;
-            }
-        }
-        System.out.println("\n");
-        System.out.println("Index of highest : " + indexOfHighest);
-        System.out.println("Value of highest: " + highest);
-        swap(intArray, indexOfHighest, intArray.length - highAdjustment);
-        System.out.println(Arrays.toString(intArray));
-
-        //getLowest2
-        for (int index = lowAdjustment; index <= intArray.length - highAdjustment; index++) {
-            if (intArray[index] <= lowest) {
-                lowest = intArray[index];
-                indexOfLowest = index;
-            }
-        }
-        System.out.println("\n");
-        System.out.println("Index of lowest : " + indexOfLowest);
-        System.out.println("Value of lowest: " + lowest);
-        swap(intArray, indexOfLowest, lowAdjustment);
-        System.out.println(Arrays.toString(intArray));
-        System.out.println ("--------------------------------");
-        
-        lowAdjustment++;
-        highAdjustment++;
-        indexOfHighest = 0;
-        indexOfLowest = 0;
-        highest = intArray[intArray.length / 2];
-        lowest = intArray[intArray.length / 2];
-        //getHighest3
-        for (int index = lowAdjustment; index <= intArray.length - highAdjustment; index++) {
-System.out.println ("INDEX: "+ index);
-            if (intArray[index] >= highest) {
-                highest = intArray[index];
-                indexOfHighest = index;
-            }
-        }
-        System.out.println("\n");
-        System.out.println("Index of highest : " + indexOfHighest);
-        System.out.println("Value of highest: " + highest);
-        swap(intArray, indexOfHighest, intArray.length - highAdjustment);
-        System.out.println(Arrays.toString(intArray));
-
-        //getLowest3
-        for (int index = lowAdjustment; index <= intArray.length - highAdjustment; index++) {
-            if (intArray[index] <= lowest) {
-                lowest = intArray[index];
-                indexOfLowest = index;
-            }
-        }
-        System.out.println("\n");
-        System.out.println("Index of lowest : " + indexOfLowest);
-        System.out.println("Value of lowest: " + lowest);
-        swap(intArray, indexOfLowest, lowAdjustment);
-        System.out.println(Arrays.toString(intArray));
-
+        //prints sorted array out at the end
+        System.out.println("Sorted Array: " + Arrays.toString(intArray));
 
     }
 
+    /**
+     * @param array the array in which its elements are being swapped
+     * @param displacingPosition the element of the array that is highest/lowest
+     * and "kicks another element out"
+     * @param displacedPosition the element of the array being "kicked out" and
+     * sent to the displacedPosition spot
+     */
     public static void swap(int[] array, int displacingPosition, int displacedPosition) {
         int holder = array[displacedPosition];
         array[displacedPosition] = array[displacingPosition];
         array[displacingPosition] = holder;
     }
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        int[] intArray = {3, 5, 6, 1, 4, 2};
+        int[] intArray = {3, 5, 6, 1, 4, 2,};
 
         sandwichSort(intArray);
     }
