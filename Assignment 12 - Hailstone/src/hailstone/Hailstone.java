0dfcde67575c0e6aba212b2422e80b82ccbe464b
CompleteTheSquare <loyal.lugia@gmail.com>
2019-05-12 21:39:10 -0400

    Final

--- Assignment 12 - Hailstone/src/hailstone/Hailstone.java
+++ Assignment 12 - Hailstone/src/hailstone/Hailstone.java
@@ -1,7 +1,7 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+This program iterates the hailstone algorithm as seen in the Collatz conjecture
+The program has a number, x, and will process it and return the number of iterations 
+it will take for it to reach 1
  */
 package hailstone;
 
@@ -13,32 +13,41 @@ public class Hailstone {
      * @param integer is the integer user input
      */
     public static int hailstone(int integer) {
-        System.out.println("Integer:" + integer);
 
-        if (integer < 0) {
+        if (integer < 0) {//checks initial integer if negative or not 
             return 0;
         }
 
-        if (integer < Integer.MIN_VALUE || integer > Integer.MAX_VALUE) {
-            return -1;
-        }
-
-        if (integer % 2 == 0) {
+        if (integer % 2 == 0) {//does algorithm
             integer = integer / 2;
+            counter++;
+            //System.out.println(counter + ": " + integer);          //to check
 
         } else if (integer % 2 == 1) {
             integer = 3 * integer + 1;
+            counter++;
+            //System.out.println(counter + ": " + integer);          //to check
         }
-        counter++;
-        if (integer != 1) {
+
+        if (integer <= 0) {//if number is larger than max num, then it will be a neg number
+            return -1;//return -1 if it is neg num
+        } else if (integer == 1) {//if int is 1, return the counter's value
+            return counter;
+        } else if (integer > 1) {//if int is greater than 1, keep iterating algorithm
             hailstone(integer);
         }
+
         return counter;
     }
 
-   
     public static void main(String[] args) {
-        System.out.println("number of iterations: " + hailstone(100));
-    }
+        int x = 100; //x is the initial number
+        System.out.println("initial integer: " + x);
+        if (hailstone(x) >= 1) {//prints number of iterations if successsful
+            System.out.println("number of iterations: " + hailstone(x));
+        } else if (hailstone(x) > 1) {
+            System.out.println(hailstone(x));//prints error code
+        }
 
+    }
 }
